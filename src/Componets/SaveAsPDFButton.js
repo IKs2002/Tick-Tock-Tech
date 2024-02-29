82ed9731a0fa1bb502677f3cf76d9dafec885b87
Amp2k2 <aden.m.perez@gmail.com>
2024-02-28 19:00:08 -0600

    Latest

--- src/Componets/SaveAsPDFButton.js
+++ src/Componets/SaveAsPDFButton.js
@@ -6,33 +6,47 @@ import './SaveAsPDFButton.css';
 
 const SaveAsPDFButton = forwardRef((props, ref) => {
   const saveAsPDF = async () => {
+    // Create a loading overlay
+    const overlay = document.createElement('div');
+    overlay.style.position = 'fixed';
+    overlay.style.top = 0;
+    overlay.style.left = 0;
+    overlay.style.width = '100%';
+    overlay.style.height = '100%';
+    overlay.style.backgroundColor = 'rgba(255, 255, 255)';
+    overlay.style.display = 'flex';
+    overlay.style.justifyContent = 'center';
+    overlay.style.alignItems = 'center';
+    overlay.innerHTML = '<div class="loader"></div>'; // Add your own loading spinner here
+  
+    // Append the overlay to the body
+    document.body.appendChild(overlay);
+  
     // Add a class to the element before capturing it
     ref.current.classList.add('print-mode');
-
+  
     const canvas = await html2canvas(ref.current, {
-      // Set additional margin (adjust as needed)
       y: -20,
     });
-
+  
     const imgData = canvas.toDataURL('image/png');
     const pdf = new jsPDF({
       orientation: 'portrait',
       format: 'a4',
     });
-
-    // Calculate new canvas dimensions with top margin
+  
     const imgProps = pdf.getImageProperties(imgData);
     const pdfWidth = pdf.internal.pageSize.getWidth();
     const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-
-    // Add image to PDF
+  
     pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
-
-    // Save the PDF
+  
     pdf.save('TimeChart.pdf');
-
-    // Remove the class after capturing the element
+  
     ref.current.classList.remove('print-mode');
+  
+    // Remove the overlay after the PDF has been saved
+    document.body.removeChild(overlay);
   };
 
   return (
