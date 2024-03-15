22ef44affdba3ca959e8df8e2440fcfce03264fd
sxerra <160073135+sxerra@users.noreply.github.com>
2024-03-15 16:40:43 -0500

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const server = express();
 
+app.listen(5000, () => console.log("Server is running"));
 
 
-application.listen(5000)
+const mongoose = require("mongoose");
 
-//update
+//application.listen(5000)
