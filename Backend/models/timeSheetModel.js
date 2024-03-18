09a3c519798bae7c8095dd62333f12650226d4c3
Ruby Koval <rtkoval42@tntech.edu>
2024-03-18 14:57:33 -0500

    Starting Server Work
    This is what I got to run a server on my machine so far, It is definitely not finished!!

---
+++ Backend/models/timeSheetModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const timesheetSchema = new mongoose.Schema({
+    day: String,
+    date: Date,
+    project: String,
+    regular: Number,
+    overtime: Number, 
+    clockIn1: String, 
+    clockOut1: String, 
+    clockIn2: String, 
+    clockOut2: String, 
+    clockIn3: String, 
+    clockOut3: String
+});
--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,35 @@
+require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const server = express();
+const app = express();
+const Timesheet = require('./models/timeSheetModel.js')
+const mongoose = require('mongoose');
+
 
 app.listen(5000, () => console.log("Server is running"));
 
 
-const mongoose = require("mongoose");
 
-//application.listen(5000)
+
+//Get request for Timesheet infomration 
+//testing connection
+
+mongoose.connect("mongodb://ddfRoot:ddfRoot@3.142.252.64:27017/?authSource=timeSheetDB", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected…'))
+  .catch(err => console.log(err));
+
+// Define the GET request endpoint for timesheet
+app.get('/timeData/:employee_id', (req, res) => {
+  const employeeId = req.params.employee_id;
+  
+  Timesheet.find({ employeeID: employeeId }, (err, timesheets) => { // employeeID is database, employeeId is variable
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(timesheets);
+    }
+  });
+});
+
+
+
