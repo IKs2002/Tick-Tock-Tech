1a1e1846d8eb114211d2b8ec0058afa79d72f619
Amp2k2 <aden.m.perez@gmail.com>
2024-03-19 19:21:09 -0500

    latest

--- Backend/controllers/timesheet-controller.js
+++ Backend/controllers/timesheet-controller.js
@@ -1,22 +1,27 @@
+const mongoose = require('mongoose');
 const Timesheet = require("../models/TimesheetModel");
 
 
 const getTimeData = async (req, res, next) => {
-    const employeeId = req.params.employee_id;
-    console.log(employeeId);
-    let timesheet
+    const tid = new mongoose.Types.ObjectId(req.params.tid);
+    const idExists = await Timesheet.exists({ _id: tid });
+    if (!idExists) {
+        console.log(`No timesheet found for ID ${tid}`);
+    }
+    console.log(tid);
+    let timesheet;
     try {
-    timesheet = await Timesheet.findById(employeeId) 
+        timesheet = await Timesheet.findById(tid);
     }
     catch (err) {
         return next(err);
     }
 
     if (!timesheet) {
-        return res.status(404).json({ message: "No timesheet found for this employee ID" });
+        return res.status(404).json({ message: "No timesheet found for this ID" });
     }
 
-    res.json({ timesheet: timesheet.toObject( {getters: true}) });
+    res.json({ timesheet: timesheet.toObject({ getters: true }) });
 };
 
-  exports.getTimeData = getTimeData;
+exports.getTimeData = getTimeData;
--- Backend/routes/timesheet-routes.js
+++ Backend/routes/timesheet-routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const Timesheet = require("../models/TimesheetModel");
+
 const timesheetcontroller = require("../controllers/timesheet-controller.js");
 
 router.post("/timesheet", );
 
-router.get("/timeData/:employee_id", timesheetcontroller.getTimeData );
+router.get("/timeData/:tid", timesheetcontroller.getTimeData );
 
 router.put("/timesheet", );
 
