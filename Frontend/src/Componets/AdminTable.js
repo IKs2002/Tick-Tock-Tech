be2d16b2bd9d5f076475ad779860861c419b26d1
Seif Ikbarieh <78017720+IKs2002@users.noreply.github.com>
2024-04-02 17:10:15 -0500

    cosmetic work

--- Frontend/src/Componets/AdminTable.js
+++ Frontend/src/Componets/AdminTable.js
@@ -159,7 +159,7 @@ const AdminTable = ({ navigateToTimesheetEdit }) => {
             style={{
               display: "flex",
               alignItems: "center",
-              marginRight: "-0.745%",
+              marginRight: "-1.5%",
               cursor: "pointer", // Change cursor to pointer when hovering over the cell
             }}
             className="clickable-cell" // Add a class for additional styling
@@ -191,14 +191,14 @@ const AdminTable = ({ navigateToTimesheetEdit }) => {
               className="btn btn-link"
               onClick={() => toggleLock(emp.id)}
               style={{
-                color: emp.locked ? "#ED5E61" : "#1C2D5A",
+                color: emp.locked ? "#ED5E61" : "#708090",
                 border: "0px",
                 width: "6vw",
                 height: "4.3vh",
                 fontSize: "1.2vw",
                 fontWeight: "bold",
                 textAlign: "center",
-                marginLeft: "12%",
+                marginLeft: "7%",
                 background: "none",
               }}
             >
