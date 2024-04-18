19d202540480fc1a27c9ee7917ee1cb27c35836f
Seif Ikbarieh <78017720+IKs2002@users.noreply.github.com>
2024-04-17 19:13:46 -0500

    timesheet behavior
    when a user is deleted (archived), their email is modified. Now, the timesheets of that user also get the email update and will be sent to a unique collection on the database.

--- Backend/models/InactiveUser.js
+++ Backend/models/InactiveUser.js
@@ -1,17 +1,16 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 const inactiveUserSchema = new mongoose.Schema({
-    name:{type: String, required: true},
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    password:{type: String, required: true,},
-    // role: String,
-    // employeeID: Number,
-    status:{type: String, default:'',},
-    accessLock:{type: Boolean, default: true}
+  name:{type: String, required: true},
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password:{type: String, required: true,},
+  role: {type: String},
+  status:{type: String, default:'',},
+  accessLock:{type: Boolean, default: false},
+  
 });
 
 const InactiveUser = mongoose.model('InactiveUser', inactiveUserSchema);
